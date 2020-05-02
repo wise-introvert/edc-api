1c23c3a65a84760465edec727afb9a7e4c8fa0a5
Fardeen Panjwani <fardeen.panjwani@ucmouli.com>
2020-05-02 20:06:49 +0530

    typeorm, mysql setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(require("../ormconfig.json")),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
