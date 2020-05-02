f59375412490bd4e2d5391f208c915092b5b4a18
Fardeen Panjwani <fardeen.panjwani@ucmouli.com>
2020-05-02 23:07:29 +0530

    deploy to heroku

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
+const PORT = process.env.PORT || 5000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(9009);
-  Logger.log(`listening on port ${9009}`, 'bootstrap');
+  app.enableCors();
+  await app.listen(PORT);
 }
 bootstrap();
