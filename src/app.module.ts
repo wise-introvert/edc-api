7e23ec7a7cd6c65f247227462daa4ca519a18b8f
Fardeen Panjwani <fardeen.panjwani@ucmouli.com>
2020-05-02 20:16:58 +0530

    removed legacy junk

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(require("../ormconfig.json")),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
