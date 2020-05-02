613413c0115fcef096f25e4c93e4a5800ab0dad9
Fardeen Panjwani <fardeen.panjwani@ucmouli.com>
2020-05-02 20:19:37 +0530

    generate module, service and controller for Center

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { CenterModule } from './center/center.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(require("../ormconfig.json")),
+    CenterModule,
   ],
 })
 export class AppModule {}
